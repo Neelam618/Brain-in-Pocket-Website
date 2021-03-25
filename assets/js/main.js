53a2c2fbc115aa0781df73819ad8cbb38c88a357
Viraj Chavan <virajc@live.com>
2021-03-25 11:46:20 +0530

    Add testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,13 +170,70 @@
     time: 1000
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
+$(document).ready(function() {
+
+  $('.owl-carousel').owlCarousel({
+    mouseDrag:false,
+    loop:true,
+    margin:2,
+    nav:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:3
+      }
+    }
   });
+
+  $('.owl-prev').click(function() {
+    let active = $('.owl-item .item.show');
+    $('.owl-item .item.show').removeClass('show');
+    $('.owl-item .item').removeClass('next');
+    $('.owl-item .item').removeClass('prev');
+    active.addClass('next');
+    if(active.is('.first')) {
+      $('.owl-item .last').addClass('show');
+      $('.first').addClass('next');
+      $('.owl-item .last').parent().prev().children('.item').addClass('prev');
+    }
+    else {
+      active.parent().prev().children('.item').addClass('show');
+      if(active.parent().prev().children('.item').is('.first')) {
+        $('.owl-item .last').addClass('prev');
+      }
+      else {
+        $('.owl-item .show').parent().prev().children('.item').addClass('prev');
+      }
+    }
+  });
+
+  $('.owl-next').click(function() {
+    let active = $('.owl-item .item.show');
+    $('.owl-item .item.show').removeClass('show');
+    $('.owl-item .item').removeClass('next');
+    $('.owl-item .item').removeClass('prev');
+    active.addClass('prev');
+    if(active.is('.last')) {
+      $('.owl-item .first').addClass('show');
+      $('.owl-item .first').parent().next().children('.item').addClass('prev');
+    }
+    else {
+      active.parent().next().children('.item').addClass('show');
+      if(active.parent().next().children('.item').is('.last')) {
+        $('.owl-item .first').addClass('next');
+      }
+      else {
+        $('.owl-item .show').parent().next().children('.item').addClass('next');
+      }
+    }
+  });
+
+});
 
   // Porfolio isotope and filter
   $(window).on('load', function() {
